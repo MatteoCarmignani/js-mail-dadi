784ac2405a8f18e42523610cee967d4b18133997
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-01 14:01:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// DADI
+
+const primoDado = Math.floor(Math.random() * 6) + 1;
+console.log(primoDado); 
+
+const secondoDado = Math.floor(Math.random() * 6) + 1;
+console.log(secondoDado); 
+
+if (primoDado > secondoDado) {
+
+  console.log("Che fortuna, il tuo dado ha vinto contro il bot con il numero " + primoDado)
+
+} else if (secondoDado > primoDado) {
+
+  console.log("Peccato, il bot ha vinto con il numero " + secondoDado)
+
+} else if (primoDado === secondoDado) {
+
+  console.log("Pareggio! Tu e il bot avete avuto lo stesso risultato")
+
+}
+
+// MAIL
+
+const mail = ["Alberto", "Alessandro", "Guglielmo", "Andrea", "Maria"]
+
+const user =prompt("Digita la tua Email");
+
+ for (let i = 0; i < mail.length; i++) {
+
+  if (user === mail[i]) {
+
+    console.log("Benvenuto " + user + ", goditi la festa")
+
+  } else if (user !== mail[i]) {
+
+    console.log("Mi dispiace " + user + " ma non sei stato invitato alla festa")
+
+  }
+
+ }
